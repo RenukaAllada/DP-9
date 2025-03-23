e3f47f865279627332a5e7d63f97f3547ef281a2
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-03-22 19:16:37 -0500

    Completed 2 problems

--- sample.java
+++ sample.java
@@ -70,5 +70,47 @@ class Sample{
     }
 
     /*******************PROBLEM-2********************/
+//TC:0(N LOG N)
+//SC:0(N)
+    class Solution {
+        public int maxEnvelopes(int[][] envelopes) {
+            if(envelopes==null || envelopes.length==0){
+                return 0;
+            }
+            int n=envelopes.length,len=1;
+            int[] result=new int[n];
 
+            Arrays.sort(envelopes,(a,b)->{
+                if(a[0]==b[0]){
+                    return b[1]-a[1];
+                }
+                return a[0]-b[0];
+            });
+            result[0]=envelopes[0][1];
+            for(int i=1;i<n;i++){
+                if(envelopes[i][1]>result[len-1]){
+                    result[len]=envelopes[i][1];
+                    len++;
+                }else{
+                    int bSearch=binarySearch(result,0,len-1,envelopes[i][1]);
+                    result[bSearch]=envelopes[i][1];
+                }
+            }
+            return len;
+        }
+
+        private int binarySearch(int[] nums,int low,int high,int target){
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }else if(nums[mid]>target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+            return low;
+        }
+    }
 }
